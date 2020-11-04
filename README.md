ac87ba221181a008008d0cd469f12ad7468b1f45
Radu-Mihai Capalb <radu_capalb98@yahoo.com>
2020-11-04 22:19:19 +0200

    Clean code and make a skeleton.
    Signed-off-by: Radu-Mihai Capalb <radu_capalb98@yahoo.com>

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# matrix
-This project is going to implement a solution for computing the inference for patients suspected of COVID.
+# Inference for Covid suspected pacients
+This project implements a solution for computing the inference for patients suspected of Covid.
 
-# Coding style
-In order to check your code against good coding style practices, you can run:
+## Project dependencies
+To install the dependencies needed to run the project, from the project's root folder run:
 ```
-pycodestyle src
+pip3 install -r requirements.txt
+```
+
+## Coding style
+To check the code against good coding style practices, from the project's root folder run:
+```
+pycodestyle covid_inference
 ```
---
+++ covid_inference/model.py
@@ -0,0 +1,40 @@
+import sys
+import data_processor
+import data_cleaner
+import data_encoder
+import model_trainer
+import model_evaluator
+
+
+def main():
+    file = sys.argv[1]
+
+    dataset = data_processor.read_file(file)
+    training_dataset, validation_dataset, testing_dataset = \
+        data_processor.split_dataset(dataset)
+
+    train(training_dataset, validation_dataset)
+    inference = infer(testing_dataset.getFeatures())
+
+    evaluation = model_evaluator.evaluate(inference, testing_dataset.getResults())
+
+
+def train(training_dataset, validation_dataset):
+    training_dataset = data_cleaner.clean(training_dataset)
+    validation_dataset = data_cleaner.clean(validation_dataset)
+
+    training_dataset = data_encoder.encode(training_dataset)
+    validation_dataset = data_encoder.encode(validation_dataset)
+
+    model_trainer.train(training_dataset)
+    inference = model_trainer.predict(validation_dataset.getFeatures())
+
+    evaluation = model_evaluator.evaluate(inference, validation_dataset.getResults())
+
+
+def infer(features):
+    pass
+
+
+if __name__ == '__main__':
+    main()
