91c1a0bf270102927d6852f40fa340b2dbef9fda
Radu Capalb <36372694+raduklb@users.noreply.github.com>
2020-11-17 07:21:53 +0200

    Train the model and evaluate it. (#15)
    Signed-off-by: Radu-Mihai Capalb <radu_capalb98@yahoo.com>

--- covid_inference/model.py
+++ covid_inference/model.py
@@ -23,8 +23,7 @@ def train(training_ds, validation_ds):
     training_ds = data_encoder.encode(training_ds)
     validation_ds = data_encoder.encode(validation_ds)
 
-    model_trainer.train(training_ds)
-    inference = model_trainer.predict(validation_ds.getFeatures())
+    inference = model_trainer.train_and_predict(training_ds, validation_ds, 1)
 
     evaluation = model_evaluator.evaluate(inference, validation_ds.getResults())
     print(evaluation)
--- covid_inference/model_trainer.py
+++ covid_inference/model_trainer.py
@@ -1,9 +1,15 @@
-model = ''
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 
-def train(training_dataset):
-    return training_dataset
+def train_and_predict(training_ds, validation_ds, algorithm):
+    if algorithm == 1:
+        model = DecisionTreeClassifier(random_state=0)
+    elif algorithm == 2:
+        model = KNeighborsClassifier()
 
+    model.fit(training_ds.getFeatures(), training_ds.getResults())
 
-def predict(features):
-    return features
+    prediction = model.predict(validation_ds.getFeatures())
+
+    return prediction
